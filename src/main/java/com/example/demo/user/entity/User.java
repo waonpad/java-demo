0d9dc332ccbc49dfe0d5529142a6c64684576b8c
waonpad <waonpad@gmail.com>
2023-05-11 14:57:23 +0900

    0511

--- src/main/java/com/example/demo/user/entity/User.java
+++ src/main/java/com/example/demo/user/entity/User.java
@@ -18,7 +18,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
+// import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
--- src/main/java/com/example/demo/user/repository/UserRepository.java
+++ src/main/java/com/example/demo/user/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.example.demo.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+// import org.springframework.stereotype.Repository;
 
 import com.example.demo.user.entity.User;
 
-@Repository
+// @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	public User findByUsername(String username);
